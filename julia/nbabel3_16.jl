bbd748d3bdf01593df8e87fea818b6cba486f771
Andrey Oskin <skoffer@arkoniak.com>
2021-01-18 15:56:56 +0200

    Prep for 16k version

--- julia/nbabel3_16.jl
+++ julia/nbabel3_16.jl
@@ -17,7 +17,7 @@ using Printf
 using DelimitedFiles
 
 tbeg = 0.0
-tend = 10.0
+tend = 0.2
 dt = 0.001
 
 function NBabel(fname::String; tend = tend, dt = dt, show=false)
@@ -32,11 +32,8 @@ function NBabel(fname::String; tend = tend, dt = dt, show=false)
 end
 
 function NBabelCalcs(ID, mass, pos, vel, tend = tend, dt = dt, show=false)
-    k = Threads.nthreads()
-    splits = splitter(size(pos, 1), k)
-    accs = [similar(vel) for _ in 1:k]
     acc = similar(vel)
-    acc = compute_acceleration(pos, mass, acc, splits, accs)
+    acc = compute_acceleration(pos, mass, acc)
     last_acc = copy(acc)
 
     Ekin, Epot = compute_energy(pos, vel, mass)
@@ -51,7 +48,7 @@ function NBabelCalcs(ID, mass, pos, vel, tend = tend, dt = dt, show=false)
 
         acc, last_acc = last_acc, acc
 
-        acc = compute_acceleration(pos, mass, acc, splits, accs)
+        acc = compute_acceleration(pos, mass, acc)
 
         vel = update_velocities(vel, acc, last_acc, dt)
         
@@ -77,7 +74,7 @@ end
 function update_positions(pos, vel, acc, dt)
     N = length(pos)
 
-    @inbounds @simd for i in 1:N
+    @fastmath @inbounds @simd for i in 1:N
         pos[i] = @. (0.5 * acc[i] * dt + vel[i])*dt + pos[i]
     end
 
@@ -87,7 +84,7 @@ end
 function update_velocities(vel, acc, last_acc, dt)
     N = length(vel)
 
-    @inbounds @simd for i in 1:N
+    @fastmath @inbounds @simd for i in 1:N
         vel[i] = @. vel[i] + 0.5 * dt * (acc[i] + last_acc[i])
     end
 
@@ -97,23 +94,18 @@ end
 """
 Force calculation.
 """
-function splitter(n, k)
-    xz = [0; reverse([Int(ceil(n*(1 - sqrt(i/k)))) for i in 1:k-1]); n]
-    return [xz[i]+1:xz[i+1] for i in 1:k]
-end
-
-function chunk_compute_acceleration(r, pos, mass, acc)
+function compute_acceleration(pos, mass, acc)
     N = length(pos)
     @inbounds for i in 1:N
         acc[i] = (0.0, 0.0, 0.0, 0.0)
     end
 
-    @inbounds for i in r
+    @inbounds for i in 1:N
 
         pos_i = pos[i]
         m_i = mass[i]
 
-        @inbounds @simd for j = i+1:N
+        @fastmath @inbounds @simd for j = i+1:N
             dr = pos_i .- pos[j]
 
             rinv3 = 1/sqrt(sum(dr .^ 2)) ^ 3
@@ -121,30 +113,6 @@ function chunk_compute_acceleration(r, pos, mass, acc)
 
             acc[i] = @. acc[i] - mass[j] * dr
             acc[j] = @. acc[j] + m_i * dr
-        end
-    end
-
-    return acc
-end
-
-function compute_acceleration(pos, mass, acc, splits, accs)
-    k = Threads.nthreads()
-    # tasks = Vector{Task}(undef, k - 1)
-    # for i in 1:k-1
-    @sync for i in 1:k
-        @async @Threads.spawn chunk_compute_acceleration(splits[i], pos, mass, accs[i])
-        # tasks[i] = @Threads.spawn chunk_compute_acceleration(splits[i], pos, mass, accs[i])
-    end
-    # chunk_compute_acceleration(splits[end], pos, mass, accs[end])
-    # for i in 1:k-1
-    #     wait(tasks[i])
-    # end
-
-    acc .= accs[1]
-    @inbounds @simd for i in 2:k
-        accs_i = accs[i]
-        for j in eachindex(acc)
-            acc[j] = acc[j] .+ accs_i[j]
         end
     end
 
@@ -169,7 +137,7 @@ function compute_energy(pos, vel, mass)
 
     @inbounds for i = 1:N-1
         pos_i = pos[i]
-        @inbounds for j = i+1:N
+        @fastmath @inbounds for j = i+1:N
             dr = pos_i .- pos[j]
 
             rinv = 1/sqrt(sum(dr .^ 2))
